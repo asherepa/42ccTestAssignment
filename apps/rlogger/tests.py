e8993e19a2969349b1656df02ce3168cc3c9ded0
Andriy Sherepa <asherepa@gmail.com>
2013-06-16 22:46:10 +0300

    Add tests for the page request

--- apps/rlogger/tests.py
+++ apps/rlogger/tests.py
@@ -1,16 +1,25 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
-
-Replace this with more appropriate tests for your application.
-"""
-
 from django.test import TestCase
+from django.test import Client
+
+REQUEST_PAGE_MAX_EVENTS = 10
 
 
 class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+
+    def test_request_page(self):
+        c = Client()
+        response = c.get('/request/')
+        events_list = response.context['events']
+        first_request_count = events_list.count()
+        response = c.get('/request/')
+        events_list = response.context['events']
+        self.assertEqual(events_list.count(),
+            first_request_count + 1)
+
+    def test_request_page_max_entry(self):
+        c = Client()
+        for i in xrange(1, 12):
+            response = c.get('/request/')
+        events_list = response.context['events']
+        self.assertEqual(events_list.count(),
+            REQUEST_PAGE_MAX_EVENTS)
