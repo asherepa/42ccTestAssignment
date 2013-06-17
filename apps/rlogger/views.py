928d8f2aea0839b35e54bd8321e55fc5ecdfeb10
Andriy Sherepa <asherepa@gmail.com>
2013-06-17 08:39:57 +0300

    Ticket #2 is solved
    Add rlogger app which can log, store and show request

--- apps/rlogger/views.py
+++ apps/rlogger/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.views import generic
+
+from apps.rlogger.models import RequestLogger
+
+
+class index(generic.ListView):
+
+    template_name = 'rlogger/index.html'
+    context_object_name = 'events'
+
+    def get_queryset(self):
+        return RequestLogger.objects.all().order_by('created_on')[:10]
