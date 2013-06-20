a979c0ea127bac1f96fc5ea9d90e5625b4c7ac5b
Andriy Sherepa <asherepa@gmail.com>
2013-06-20 10:16:57 +0300

    ticket:5 Write test for checking login and logout procedure

--- apps/accounts/tests.py
+++ apps/accounts/tests.py
@@ -4,6 +4,7 @@ from django.test import TestCase
 
 
 USER_NAME = 'Andriy'
+USER_PASSWORD = 'testdata'
 USER_NEW_NAME = 'King-Kong-Dong'
 USER_SURNAME = 'Sherepa'
 USER_DATE_OF_BIRTH = date(1986, 10, 30)
@@ -12,13 +13,12 @@ USER_BIO = "It's my life\r\nIt's now or never\r\nI ain't gonna live forever"\
 USER_JABBER_JID = 'dustin@jabber.ru'
 USER_SKYPE_ID = 'a.sherepa'
 USER_OTHER_CONTACTS = 'Location: Kiev\r\nPhone: +380675976116'
+APP_MAIN_PAGE = reverse('accounts:index')
 
 
 class SimpleTest(TestCase):
-
     def test_main_page_context_data(self):
-
-        response = self.client.get(reverse('accounts:index'))
+        response = self.client.get(APP_MAIN_PAGE)
         profile = response.context['profile']
         self.assertEqual(response.status_code, 200)
         self.assertEqual(profile.first_name, USER_NAME)
@@ -28,3 +28,22 @@ class SimpleTest(TestCase):
         self.assertEqual(profile.jid, USER_JABBER_JID)
         self.assertEqual(profile.skype_id, USER_SKYPE_ID)
         self.assertEqual(profile.other_contacts, USER_OTHER_CONTACTS)
+
+
+class AuthTest(TestCase):
+    def test_login_success(self):
+        response = self.client.get(APP_MAIN_PAGE)
+        self.assertTrue(response.status_code, 200)
+        self.assertTrue(self.client.login(username=USER_NAME,
+                                          password=USER_PASSWORD))
+        response = self.client.get(reverse('accounts:edit'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Logout')
+        self.assertEqual(self.client.logout())
+
+    def test_unauth_request_edit_page(self):
+        response = self.client.get(APP_MAIN_PAGE)
+        self.assertContains(response, 'Login')
+        response = self.client.get(reverse('accounts:edit'))
+        self.assertRedirects(response,
+                             '/accounts/login/?next=/accounts/edit/')
